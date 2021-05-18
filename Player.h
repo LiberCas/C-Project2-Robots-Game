3ed9113842f257c703eb8cab03b473055a21e99e
Lucas Nakajima <82966885+Lucasnakajima@users.noreply.github.com>
2021-05-18 16:53:28 +0100

    Create Player.h

---
+++ Player.h
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+struct Movement
+{
+	int dRow, dCol; // displacement, taking into account the chosen movement
+};
+
+class Player {
+public:
+	Player(int row, int col, char symbol);
+	int getRow() const;
+	int getCol() const;
+	char getSymbol() const;
+	bool isAlive() const;
+	void setAsDead();
+	bool move(Movement delta);
+private:
+	int row, col;
+	bool alive;
+	char symbol;
+};
