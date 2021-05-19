66f014409a2a1ddc727a7843cf250fde86408639
Lucas Nakajima <82966885+Lucasnakajima@users.noreply.github.com>
2021-05-19 11:01:42 +0100

    Add files via upload

---
+++ Post.h
@@ -0,0 +1,16 @@
+#include "Misc.h"
+
+class Post {
+public:
+	Post(int row, int col, char type);
+	int getRow() const;
+	int getCol() const;
+	char getSymbol() const; // get char representation of Post
+	bool isElectrified() const;
+	//other methods
+private:
+	int row, col;
+	char type; // '*' - electrified; '+'- non-electrified
+	// other attributes (?)
+	// could also have a state, like the robot(?)
+}; 
